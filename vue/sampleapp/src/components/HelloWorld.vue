684c5486a2cbc8fa11d63bac53013b01595fe3c3
Mochi <whoatemyapplepie@gmail.com>
2022-11-26 23:54:47 +0900

    add code to test breakpoints in components.

--- vue/sampleapp/src/components/HelloWorld.vue
+++ vue/sampleapp/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ testString }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -123,6 +124,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HelloWorld",
+  data() {
+    return {
+      testString: "hogehoge----",
+    };
+  },
   props: {
     msg: String,
   },
